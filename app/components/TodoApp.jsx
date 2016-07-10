1219c66146e87d06f2780994354412d9c5ae304b
Justin Bennett <fortitude1981@gmail.com>
2016-07-09 20:03:40 -0500

    add completed and created timestamps

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,11 @@
 var React = require('react');
+var uuid = require('node-uuid');
+var moment = require('moment');
+
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
-var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -16,25 +18,30 @@ var TodoApp = React.createClass({
   componentDidUpdate: function () {
     TodoAPI.setTodos(this.state.todos);
   },
-  handleAddTodo: function(text) {  //we wanna pass this down as the prop
+  handleAddTodo: function (text) {
     this.setState({
       todos: [
         ...this.state.todos,
         {
           id: uuid(),
           text: text,
-          completed: false
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
         }
       ]
     });
   },
-  handleToggle: function(id) {
+  handleToggle: function (id) {
     var updatedTodos = this.state.todos.map((todo) => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
       }
+
       return todo;
     });
+
     this.setState({todos: updatedTodos});
   },
   handleSearch: function (showCompleted, searchText) {
@@ -43,14 +50,14 @@ var TodoApp = React.createClass({
       searchText: searchText.toLowerCase()
     });
   },
-  render: function() {
-    var {todos, showCompleted, searchText} = this.state; //pull off state using es6 destructuring
-    var filterTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+  render: function () {
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={filterTodos} onToggle={this.handleToggle}/>
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     )
